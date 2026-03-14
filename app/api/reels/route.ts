99a34f5a53a003295c2f8967f43aa2b52b8d9bc1
dev-abdullah11 <abdullahworld111@gmail.com>
2026-03-14 23:49:50 +0500

    feat: Add GET endpoint to retrieve video reels with authorization check

---
+++ app/api/reels/route.ts
@@ -0,0 +1,35 @@
+import { connectDB } from "@/lib/mongodb";
+import { authOptions } from "@/lib/nextAuth";
+import Post from "@/Models/post.Model";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectDB();
+        const session = await getServerSession(authOptions);
+        if (!session?.user.id) {
+            return NextResponse.json(
+                { message: "Unauthorized access" },
+                { status: 401 },
+            );
+        }
+
+        const reels = await Post.find({ "media.type": "video" });
+        if (!reels || reels.length === 0) {
+            return NextResponse.json(
+                { message: "Reels not found" },
+                { status: 404 },
+            );
+        }
+
+        return NextResponse.json({ message: "Reels found successfully", totalReels: reels.length, reels }, { status: 200 });
+
+    } catch (error) {
+        console.error("Error fetching reels:", error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 },
+        );
+    }
+}
