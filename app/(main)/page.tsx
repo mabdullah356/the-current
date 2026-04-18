37cb10ea9dce271d07494b5487f8025dd38a32eb
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-18 09:19:14 +0500

    feat: check server session and redirect to login in main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,15 @@
 import AsideBar from "@/Components/AsideBar";
 import Posts from "@/Components/Posts";
 import Stories from "@/Components/Stories";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/nextAuth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <main className=" flex w-full h-full md:gap-4 overflow-hidden">
       <section className="w-full md:w-2/3 ">
