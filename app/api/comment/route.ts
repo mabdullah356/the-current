2685ecb83899791aa91c317dd92b7fe0555e57d0
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-19 09:10:01 +0500

    api (comment) - to create new comment

---
+++ app/api/comment/route.ts
@@ -0,0 +1,34 @@
+import { connectDB } from "@/lib/mongodb";
+import { authOptions } from "@/lib/nextAuth";
+import Comment from "@/Models/comment.Model";
+import Post from "@/Models/post.Model";
+import { getServerSession } from "next-auth";
+import { NextRequest,NextResponse } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+    
+    if(!session){
+        return NextResponse.json({message:"unauthorized"},{status:401})
+    };
+
+    try {
+        await connectDB();
+
+        const {postId,content} = await request.json();
+
+        const post = await Post.findById(postId);
+        if(!post){
+            return NextResponse.json({message:"POST not found"},{status:404})
+        };
+
+        const comment  = await Comment.create({userId:session.user.id,postId,content});
+
+        return NextResponse.json({message:"Comment created successfully",comment},{status:201})
+        
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message:"Internal server error"},{status:500});
+    }
+}
