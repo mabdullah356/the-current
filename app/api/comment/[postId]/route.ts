7bafc3bdebac7b6f1be987d6831eeaddd846404b
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-19 10:12:28 +0500

    api (comment) - fetch all comment by postId

---
+++ app/api/comment/[postId]/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest,NextResponse } from "next/server";
+import Post from "@/Models/post.Model";
+import Comment from "@/Models/comment.Model";
+
+export async function GET(request: NextRequest, { params }: { params: { postId: string } }) {
+  try {
+    const { postId } = await params;
+    
+    if (!postId) {
+      return NextResponse.json({ message: "Post ID is required" }, { status: 400 });
+    };
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return NextResponse.json({ message: "Post not found" }, { status: 404 });
+    };
+
+    const comments  = await Comment.find({ postId }).sort({ createdAt: -1 });
+
+    return NextResponse.json({ message: "Comment created successfully", totalComments: comments.length , comments }, { status: 201 });
+  
+} catch (error) {
+
+    console.error(error);
+    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+
+}}
