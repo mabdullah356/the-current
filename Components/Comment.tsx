956189fdae5d3af4fd027209fd1c47648524e095
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-20 12:21:05 +0500

    feat(comment) : implement the delete functionality of comments

--- Components/Comment.tsx
+++ Components/Comment.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import axios from "axios";
+import { useSession } from "next-auth/react";
 
 export type CommentType = {
   _id: string;
@@ -16,7 +17,25 @@ export type CommentType = {
   };
 };
 
-export function Comment({ comment }: { comment: CommentType }) {
+export function Comment({ comment ,postId}: { comment: CommentType; postId: string }) {
+  const {data:session} = useSession();
+  const [loading,setLoading] = useState(false);
+
+  const handleDeleteComment = async (commentId: string)=>{
+  
+    try {
+      setLoading(true);
+      await axios.delete(`/api/comment/${postId}/comments/${commentId}`);
+        alert("Comment deleted successfully!");
+        comment.content = "This comment has been deleted.";
+    } catch (error) {
+        console.error("Error deleting comment:", error);
+    } finally {
+        setLoading(false);
+    }
+  };
+
+
   return (
     <div className="flex items-start gap-3 py-3">
       <Image
@@ -37,7 +56,6 @@ export function Comment({ comment }: { comment: CommentType }) {
             {comment.content}
           </p>
         </div>
-
         <div className="flex items-center gap-2 text-xs text-zinc-500">
           <span>
             {new Date(comment.createdAt).toLocaleString()}
@@ -47,6 +65,14 @@ export function Comment({ comment }: { comment: CommentType }) {
           )}
         </div>
       </div>
+      {comment.userId._id === session?.user.id && (
+        <button 
+        onClick={()=>handleDeleteComment(comment._id)}
+        className="bg-red-500 hover:bg-red-600 px-2 py-1 rounded-lg text-sm font-medium">
+          {loading ? "deleting... " : "Delete"}
+        </button>
+      )}
+        
     </div>
   );
 }
@@ -71,7 +97,7 @@ export function CommentsList({
         <p className="text-sm text-zinc-500 text-center py-4">No comments yet. Be the first to comment!</p>
        ) : (      
       commentList.map((c) => (
-        <Comment key={c._id} comment={c} />
+        <Comment key={c._id} comment={c} postId={postId} />
       ))
        )}
     </div>
