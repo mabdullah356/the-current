614dfbe08392732c7ed8b2737885f87191f3c103
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-20 12:45:36 +0500

    api (story) -delete route for story by id!

---
+++ app/api/stories/[id]/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server"
+import Story from '@/Models/story.Model'; 
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/nextAuth";
+
+export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    
+    const session = await getServerSession(authOptions);
+    if(!session){
+        return NextResponse.json({message:"Unauthorized"},{status:401})
+    };
+    
+    const {id} = await params
+    if(!id){
+        return NextResponse.json({message:"Story ID is required!"},{status:400})
+    };
+
+    try {
+        
+        const story = await Story.findById(id);
+    if(!story){
+        return NextResponse.json({message:"Story not found"},{status:404})
+    };
+    
+    if(story.user.toString()!==session.user.id){
+        return NextResponse.json({message:"Unauthorized"},{status:401})
+    };
+     await Story.findByIdAndDelete(id);
+
+     return NextResponse.json({message:"Story deleted successfully"},{status:200})
+    
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message:"Internal server error"},{status:500})        
+    }
+    
+
+}   
