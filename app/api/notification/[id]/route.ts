173f6c935b7c84135d7266522b27f61b4cfdb514
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-25 23:48:08 +0500

    api (create-notification) : make the api route for delete notification

---
+++ app/api/notification/[id]/route.ts
@@ -0,0 +1,50 @@
+import notificationModel from "@/Models/notification.model";
+import { NextResponse, NextRequest } from "next/server";
+import { connectDB } from "@/lib/mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/nextAuth";
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+
+   const session = await getServerSession(authOptions);
+   if(!session){
+    return NextResponse.json({message:"unauthorized"},{status:401})
+   } 
+  const { id } = await params;
+//   console.log(id)
+
+  if (!id) {
+    return NextResponse.json(
+      { message: "Notification ID is required!" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    await connectDB();
+    
+    const notification = await notificationModel.findByIdAndDelete(id);
+
+    if (!notification) {
+      return NextResponse.json(
+        { message: "Notification not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Notification deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
