2f203b80b9da6c81b548c3540ff4160c145342cc
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-05-04 10:15:04 +0500

    fix: update ShareStoryPopUp styles for improved button visibility

--- Components/Post.tsx
+++ Components/Post.tsx
@@ -290,7 +290,7 @@ export function ShareStoryPopUp({setShowSharePopup , postId}:ShareStoryPopUpProp
 
 
   return (
-    <main className="fixed top-1/3 right-6 z-999 w-full max-w-xs bg-white shadow-lg p-4 rounded-t-2xl">
+    <main className="fixed top-1/3 right-6 z-999 w-full max-w-xs shadow-lg p-4 rounded-t-2xl">
       
       <div className="flex items-center justify-between">
         <h2 className="text-lg font-semibold">
@@ -306,7 +306,7 @@ export function ShareStoryPopUp({setShowSharePopup , postId}:ShareStoryPopUpProp
       <div className="flex justify-between mt-6">
         <button 
         onClick={()=>setShowSharePopup(false)}
-        className="flex items-center gap-2 px-4 py-2 bg-gray-200 text-zinc-600 rounded-lg">
+        className="flex items-center gap-2 px-4 py-2 text-gray-200 bg-zinc-600 rounded-lg">
           <IoClose />
           Cancel
         </button>
